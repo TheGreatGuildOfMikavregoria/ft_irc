acfd4fe88cf631cf8b08a740db10500e94378909
unknown <org.grego@gmail.com>
2025-12-12 14:17:39 +0200

    Cleanup, added errors, 407 needs to be implemented.

--- cmds/Msg.cpp
+++ cmds/Msg.cpp
@@ -15,51 +15,65 @@ void Server::privmsg(Client &c, Command &cmd)
 {
 	(void)cmd;
 	auto tokens = cmd.getTokens();
-	if (tokens.size() < 3)
-		return ;
+	Buffer &outBuf = c.getOutBuf();
+
+	if (tokens.size() < 2)
+	{
+		std::string rpl = numericRPL(ERR_NORECIPIENT, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+		outBuf.append(rpl.c_str(), rpl.length());
+		return ; //411 ERR_NORECIPIENT (411)
+	}
 	auto &receiver = tokens[1];
+	
+	if (tokens.size() < 3)
+	{
+		std::string rpl = numericRPL(ERR_NOTEXTTOSEND, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+		outBuf.append(rpl.c_str(), rpl.length());
+		return ;//412 ERR_NOTEXTTOSEND (412)
+	}
 	auto &msg = tokens[2];
-	//std::string &receiver = tokens[1];
-	//std::cout << receiver<< std::endl;
-	//std::string &msg = tokens[2];
-	//for (auto token : tokens)
-	//	std::cout << ":"<<token << std::endl;
+
+	if (tokens[1].find(',') != std::string::npos)
+	{
+		//std::string rpl = numericRPL(ERR_TOOMANYTARGETS, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+//				outBuf.append(rpl.c_str(), rpl.length());
+		return ;//407 ERR_TOOMANYTARGETS (407)
+	}
 
 	if (receiver.at(0) == '#')
 	{
 		//Send to channel
-		std::set<Channel*> &userChannels = c.getUserChannels();
-		Channel *channelPtr = getChannelByName(tokens[1]);
-		//std::cout << "he2re"<< std::endl;
-
-		if (userChannels.find(channelPtr) != userChannels.end())
+		Channel *channelPtr = getChannelByName(receiver);
+		if (!channelPtr)
 		{
-			std::string msgBuild = ":" + c.getNickName() + " PRIVMSG " + receiver + " :" + msg + "\r\n";
-		//	std::cout << msg << std::endl;
-			channelPtr->chanBroadcast(c,msgBuild);
-//std::cout << "here"<< std::endl;
-			return ;
+			std::string rpl = numericRPL(ERR_NOSUCHCHANNEL, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+			outBuf.append(rpl.c_str(), rpl.length());
+			return ; //403
 		}
-		else if (tokens[1].find(','))
-			return ;//407
-		else
-			return ;//404;
+		std::set<Channel*> &userChannels = c.getUserChannels();
+		if (userChannels.find(channelPtr) == userChannels.end())
+		{
+			std::string rpl = numericRPL(ERR_CANNOTSENDTOCHAN, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+			outBuf.append(rpl.c_str(), rpl.length());
+
+			return ;//404; ERR_CANNOTSENDTOCHAN (404)
+		}
+
+		std::string payload = ":" + c.getNickName() + " PRIVMSG " + receiver + " :" + msg + "\r\n";
+		channelPtr->chanBroadcast(c,payload);
 	}
 	else
 	{
-//std::cout << "wtf";
-		if (auto target = clientLookUp(receiver))
+		auto target = clientLookUp(receiver);
+		if (!target)
 		{
-		//	std::cout << "wtf";
-
-			std::string msgBuild = ":" + c.getNickName() + " PRIVMSG " + receiver + " :" + msg + "\r\n";
-
-			target->getOutBuf().append(msgBuild.c_str(), msgBuild.size());
+			std::string rpl = numericRPL(ERR_NOSUCHNICK, c.getNickNameStatus() ? c.getNickName() : c.getUserName());
+			outBuf.append(rpl.c_str(), rpl.length());
+			return ;//401; ERR_NOSUCHNICK (401)
 		}
-		else
-			return ;//401;
-	}
 
-	std::cout << tokens[1] << std::endl;
-	auto inbuf = c.getInBuf();
+		std::string payload = ":" + c.getNickName() + " PRIVMSG " + receiver + " :" + msg + "\r\n";
+		target->getOutBuf().append(payload.c_str(), payload.size());
+	}
+	return ;
 }
